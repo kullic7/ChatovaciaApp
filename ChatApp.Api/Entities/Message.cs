2750ccba0a1bf874768d943c459d0cc898e0f9a6
rfrat <r.fratrik7@gmail.com>
2026-07-04 10:09:52 +0200

    Add API entities

---
+++ ChatApp.Api/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatApp.Api.Entities;
+public class Message
+{
+	public int Id { get; set; }
+
+	public int UserId { get; set; }
+
+	public User User { get; set; } = null!;
+
+	public string Text { get; set; } = string.Empty;
+
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}

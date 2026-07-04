de6a03ba7543016042f761195e31766741afa3b9
rfrat <r.fratrik7@gmail.com>
2026-07-04 11:45:02 +0200

    Add UserController

---
+++ ChatApp.Api/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using ChatApp.Shared.DTOs;
+using ChatApp.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+	private readonly IUserService _userService;
+
+	public UsersController(IUserService userService)
+	{
+		_userService = userService;
+	}
+
+	[HttpPost]
+	public async Task<ActionResult<UserResponse>> CreateOrGetUser(CreateUserRequest request)
+	{
+		try
+		{
+			var user = await _userService.CreateOrGetUserAsync(request);
+			return Ok(user);
+		}
+		catch (ArgumentException exception)
+		{
+			return BadRequest(exception.Message);
+		}
+	}
+}

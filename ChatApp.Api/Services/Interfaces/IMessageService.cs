c42f68512d4226eaab5614af0496186510f2e284
rfrat <r.fratrik7@gmail.com>
2026-07-04 10:44:24 +0200

    Add interfaces to Api project

---
+++ ChatApp.Api/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using ChatApp.Shared.DTOs;
+
+namespace ChatApp.Api.Services.Interfaces;
+
+public interface IMessageService
+{
+	Task<List<MessageResponse>> GetMessagesAsync(int? afterId);
+	Task<MessageResponse> CreateMessageAsync(CreateMessageRequest request);
+}
---
+++ ChatApp.Api/Services/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using ChatApp.Shared.DTOs;
+
+namespace ChatApp.Api.Services.Interfaces;
+
+public interface IUserService
+{
+	Task<UserResponse> CreateOrGetUserAsync(CreateUserRequest request);
+}
